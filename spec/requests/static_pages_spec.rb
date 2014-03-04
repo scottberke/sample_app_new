c50af4d4c2cb3bd0633d6f81bc38ebd65936ba57
scottberke <scottberke@gmail.com>
2014-03-04 15:33:36 -0700

    "Make a basic User model (including secure passwords)"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,7 +24,7 @@ describe "Static pages" do
   describe "About page" do
   	before {visit about_path}
 
-  	it { should have_selector('h1', text: 'About')}
+    it { should have_selector('h1', text: 'About')}
   	it { should have_selector'title', text: full_title('About') }
   end
 
